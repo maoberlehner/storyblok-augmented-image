0c6ad7fed1f787375dd905ea9e3b966c99247ed5
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-07-06 13:40:01 +0200

    Optimize styling

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    <input type="file" @change="uploadImage">
-
-    <div v-if="model.image">
-      <img class="image" :src="model.image.replace('a.storyblok.com', 'img2.storyblok.com/160x90/filters:fill(auto,0)')">
-      <input type="text" v-model="model.image">
-      <button @click.prevent="removeImage">Remove</button>
+  <div class="uk-placeholder uk-margin-bottom-remove uk-margin-top-remove">
+    <div
+      v-if="model.image"
+      class="uk-flex uk-flex-middle uk-margin-small-bottom"
+    >
+      <img
+        class="image"
+        :src="previewImage"
+      >
+      <input
+        class="uk-form-small uk-flex-item-auto"
+        v-model="model.image"
+      >
+      <a
+        class="image__btn"
+        target="_blank"
+        :href="model.image"
+      >
+        <i class="uk-icon-search"></i>
+      </a>
+      <a
+        class="image__btn"
+        @click.prevent="removeImage"
+      >
+        <i class="uk-icon-close"></i>
+      </a>
     </div>
+    <hr>
+    <label class="uk-form-file uk-button uk-button-small uk-button-primary">
+      <span class="util__nobreak">
+        <i class="uk-icon-upload"></i> Upload new
+      </span>
+      <input
+        class="upload-select"
+        accept="image/x-png,image/png,image/gif,image/jpeg,image/svg+xml"
+        type="file"
+        @change="uploadImage"
+      >
+    </label>
   </div>
 </template>
 
@@ -21,6 +52,11 @@ export default {
         this.$emit('changed-model', value);
       },
       deep: true,
+    },
+  },
+  computed: {
+    previewImage() {
+      return this.model.image.replace('a.storyblok.com', 'img2.storyblok.com/160x90/filters:fill(auto,0)');
     },
   },
   methods: {
