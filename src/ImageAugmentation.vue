0596cf2f8fb2e639adebd753c79c39681b74a3b6
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-07-10 18:23:05 +0200

    Move augmentation logic into separate component

---
+++ src/ImageAugmentation.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="blok__comp-augmentation">
+    <p>Click on the image to set a marker for the augmentation position.</p>
+    <div class="augmentation-picker">
+      <img
+        :src="augmentedImage.replace('a.storyblok.com', 'img2.storyblok.com/300x0')"
+        @click="setCoordinates"
+      >
+      <span
+        :style="`top:${y}%;left:${x}%;`"
+        class="augmentation-picker__marker"
+      />
+    </div>
+    <div class="uk-flex">
+      <div class="uk-flex-item-auto tree__form-group">
+        <label class="form__topic">
+          X %
+          <input
+            v-model="x"
+            class="uk-form-small"
+            @input="updateValue"
+          >
+        </label>
+      </div>
+
+      <div class="uk-flex-item-auto uk-margin-small-left tree__form-group">
+        <label class="form__topic">
+          Y %
+          <input
+            v-model="y"
+            class="uk-form-small"
+            @input="updateValue"
+          >
+        </label>
+      </div>
+    </div>
+
+    <div class="uk-flex-item-auto tree__form-group">
+      <span class="form__topic">
+        Augmentation image
+      </span>
+      <image-upload
+        v-model="image"
+        @input="updateValue"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ImageUpload from './ImageUpload.vue';
+
+export default {
+  components: {
+    ImageUpload,
+  },
+  inject: [
+    `plugin`,
+  ],
+  props: {
+    augmentedImage: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      image: ``,
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    setCoordinates(e) {
+      const { height, width } = e.target;
+
+      this.x = parseFloat((e.offsetX / (width / 100)).toFixed(2));
+      this.y = parseFloat((e.offsetY / (height / 100)).toFixed(2));
+
+      this.updateValue();
+    },
+    updateValue() {
+      this.$emit(`input`, {
+        image: this.image,
+        x: this.x,
+        y: this.y,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.blok__comp-augmentation {
+  padding: 15px;
+  border: 1px solid #d4d4d4;
+  border-radius: 3px;
+}
+
+.augmentation-picker {
+  position: relative;
+}
+
+.augmentation-picker__marker {
+  position: absolute;
+  margin-top: -8px;
+  margin-left: -8px;
+  width: 16px;
+  height: 16px;
+}
+
+.augmentation-picker__marker::before,
+.augmentation-picker__marker::after {
+  position: absolute;
+  background-color: #fff;
+  content: '';
+}
+
+.augmentation-picker__marker::before {
+  left: 7px;
+  width: 2px;
+  height: 100%;
+}
+
+.augmentation-picker__marker::after {
+  top: 7px;
+  width: 100%;
+  height: 2px;
+}
+</style>
--- src/Plugin.vue
+++ src/Plugin.vue
@@ -3,64 +3,12 @@
     <image-upload v-model="model.image"/>
 
     <div class="blok__comp-container">
-      <div
+      <image-augmentation
         v-for="(augmentation, index) in model.augmentations"
         :key="index"
-        class="blok__comp"
-      >
-        <div class="blok__comp-augmentation">
-          <p>Click on the image to set a marker for the augmentation position.</p>
-          <div class="augmentation-picker">
-            <img
-              :src="model.image.replace('a.storyblok.com', 'img2.storyblok.com/300x0')"
-              @click="setCoordinates($event.target, $event.offsetX, $event.offsetY, augmentation)"
-            >
-            <span
-              :style="`top:${augmentation.y}%;left:${augmentation.x}%;`"
-              class="augmentation-picker__marker"
-            />
-          </div>
-          <div class="uk-flex">
-            <div class="uk-flex-item-auto tree__form-group">
-              <label
-                :for="`augmentation-x-${index}`"
-                class="form__topic"
-              >
-                X %
-              </label>
-              <input
-                :id="`augmentation-x-${index}`"
-                v-model="augmentation.x"
-                class="uk-form-small"
-              >
-            </div>
-
-            <div class="uk-flex-item-auto uk-margin-small-left tree__form-group">
-              <label
-                :for="`augmentation-y-${index}`"
-                class="form__topic"
-              >
-                Y %
-              </label>
-              <input
-                :id="`augmentation-Y-${index}`"
-                v-model="augmentation.y"
-                class="uk-form-small"
-              >
-            </div>
-          </div>
-
-          <div class="uk-flex-item-auto tree__form-group">
-            <span
-              :for="`augmentation-x-${index}`"
-              class="form__topic"
-            >
-              Augmentation image
-            </span>
-            <image-upload v-model="augmentation.image"/>
-          </div>
-        </div>
-      </div>
+        :augmented-image="model.image"
+        v-model="model.augmentations[index]"
+      />
     </div>
 
     <a
@@ -73,10 +21,12 @@
 </template>
 
 <script>
+import ImageAugmentation from './ImageAugmentation.vue';
 import ImageUpload from './ImageUpload.vue';
 
 export default {
   components: {
+    ImageAugmentation,
     ImageUpload,
   },
   mixins: [window.Storyblok.plugin],
@@ -110,53 +60,6 @@ export default {
         y: 0,
       });
     },
-    setCoordinates(element, offsetX, offsetY, augmentation) {
-      const { height, width } = element;
-
-      // eslint-disable-next-line no-param-reassign
-      augmentation.x = parseFloat((offsetX / (width / 100)).toFixed(2));
-      // eslint-disable-next-line no-param-reassign
-      augmentation.y = parseFloat((offsetY / (height / 100)).toFixed(2));
-    },
   },
 };
 </script>
-
-<style>
-.blok__comp-augmentation {
-  padding: 15px;
-  border: 1px solid #d4d4d4;
-  border-radius: 3px;
-}
-
-.augmentation-picker {
-  position: relative;
-}
-
-.augmentation-picker__marker {
-  position: absolute;
-  margin-top: -8px;
-  margin-left: -8px;
-  width: 16px;
-  height: 16px;
-}
-
-.augmentation-picker__marker::before,
-.augmentation-picker__marker::after {
-  position: absolute;
-  background-color: #fff;
-  content: '';
-}
-
-.augmentation-picker__marker::before {
-  left: 7px;
-  width: 2px;
-  height: 100%;
-}
-
-.augmentation-picker__marker::after {
-  top: 7px;
-  width: 100%;
-  height: 2px;
-}
-</style>
